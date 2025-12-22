c827fd55741a276bb46c45039951bb9e03de87f2
Alaister Leung <70866538+AlaisterLeung@users.noreply.github.com>
2025-12-22 22:34:10 +0800

    Fix Restic scripts

--- rootful/restic/bin/restic-home-share.sh
+++ rootful/restic/bin/restic-home-share.sh
@@ -3,8 +3,8 @@ set -e
 
 podman run --rm -it \
     -v restic-cache:/root/.cache/restic \
-    -s restic_home_share_repository,type=env,target=RESTIC_REPOSITORY \
-    -s restic_home_share_password,type=env,target=RESTIC_PASSWORD \
-    -s restic_aws_access_key_id,type=env,target=AWS_ACCESS_KEY_ID \
-    -s restic_aws_secret_access_key,type=env,target=AWS_SECRET_ACCESS_KEY \
+    --secret restic_home_share_repository,type=env,target=RESTIC_REPOSITORY \
+    --secret restic_home_share_password,type=env,target=RESTIC_PASSWORD \
+    --secret restic_aws_access_key_id,type=env,target=AWS_ACCESS_KEY_ID \
+    --secret restic_aws_secret_access_key,type=env,target=AWS_SECRET_ACCESS_KEY \
     docker.io/restic/restic:latest "$@"
