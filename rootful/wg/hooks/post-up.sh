45274e63e37e81495477885d52e77bf53ea40daa
Alaister <70866538+AlaisterLeung@users.noreply.github.com>
2025-12-25 18:22:21 +0800

    Fix wg-easy clients’ access to server LAN

--- rootful/wg/hooks/post-up.sh
+++ rootful/wg/hooks/post-up.sh
@@ -5,8 +5,11 @@ iptables -A INPUT -p udp -m udp --dport 51820 -j ACCEPT
 iptables -A FORWARD -i wg0 -j ACCEPT
 iptables -A FORWARD -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -s 10.8.0.0/24 -d 10.8.0.0/24 -i wg0 -o wg0 -j ACCEPT
+iptables -A FORWARD -s 10.8.0.0/24 -d 192.168.0.0/24 -j ACCEPT
+iptables -A FORWARD -s 192.168.0.0/24 -d 10.8.0.0/24 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 ip rule add from 10.8.0.0/24 table 200
 ip route add default via 10.89.1.2 dev eth0 table 200
 ip route add 10.89.1.0/24 via 10.89.1.1 dev eth0 table 200
+ip route add 192.168.0.0/24 via 10.89.1.1 dev eth0 table 200
 ip route add 10.8.0.0/24 dev wg0 table 200
